41e73de63b528294ecaa76b6a36d098bfc28d5e0
Alvaro Aspee <aspeealvaro@gmail.com>
2020-04-08 18:41:21 -0400

    improving project structure

--- src/database.js
+++ src/database.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const { NOTES_APP_MONGDB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
 
-const MONGODB_URI = 'mongodb://${NOTES_APP_MONGDB_HOST}/${NOTES_APP_MONGODB_DATABASE}';
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
-mongoose
-  .connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ require('./database');
 
 // Server is listenning
 app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+    console.log('Server on port ', app.get('port'));
     console.log('Enviroment:', process.env.NODE_ENV);
 });
